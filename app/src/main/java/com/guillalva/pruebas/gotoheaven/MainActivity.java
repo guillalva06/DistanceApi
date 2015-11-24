cff0693471c8a1acaf70811f06351fe6ae75e0c9
guillalva06 <guillermo.alvarez@mail.escuelaing.edu.co>
2015-11-24 13:50:30 -0500

    Pinta la ruta en el mapa entre dos puntos

--- app/src/main/java/com/guillalva/pruebas/gotoheaven/MainActivity.java
+++ app/src/main/java/com/guillalva/pruebas/gotoheaven/MainActivity.java
@@ -1,11 +1,21 @@
 package com.guillalva.pruebas.gotoheaven;
 
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -14,29 +24,35 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    String txt;
+    private static final String TAG = MainActivity.class.toString();
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //txt = (TextView)findViewById(R.id.txt);
-        AsyncTask<String,Void,String> task = new AsyncTask<String, Void, String>() {
+        MapFragment fragment = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
+        map = fragment.getMap();
+        AsyncTask<LatLng,Void,String> task = new AsyncTask<LatLng, Void, String>() {
 
             @Override
-            protected String doInBackground(String... params) {
+            protected String doInBackground(LatLng... params) {
                 StringBuilder builder = new StringBuilder();
                 HttpClient client = new DefaultHttpClient();
-                HttpGet httpGet = new HttpGet("https://maps.googleapis.com/maps/api/directions/json?origin=Chicago,IL&destination=Joplin,MO&key=AIzaSyDnnmgi40JjFpgC_tEywJ07ZZQiaoQ3Swk");
+                HttpGet httpGet = new HttpGet(getUrl(params[0],params[1]));
                 System.out.println(httpGet.getURI());
                 try {
                     HttpResponse response = client.execute(httpGet);
@@ -66,10 +82,34 @@ public class MainActivity extends ActionBarActivity {
             protected void onPostExecute(String s) {
                 super.onPostExecute(s);
                 //txt.setText(s);
-                Log.i("TAG ",s);
+                Log.i(TAG,s);
+                ParserTask parserTask = new ParserTask();
+
+                // Invokes the thread for parsing the JSON data
+                parserTask.execute(s);
             }
         };
-        task.execute();
+        task.execute(new LatLng(4.780923,-74.0476033),new LatLng(4.3364751,-74.3697546));
+        CameraPosition camera = new CameraPosition.Builder()
+                .zoom(13)
+                .target(new LatLng(4.780923,-74.0476033))
+                .build();
+        map.moveCamera(CameraUpdateFactory.newCameraPosition(camera));
+    }
+
+
+    private String getUrl(LatLng origen, LatLng destino){
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("https")
+                .authority("maps.googleapis.com")
+                .appendPath("maps")
+                .appendPath("api")
+                .appendPath("directions")
+                .appendPath("json")
+                .appendQueryParameter("origin",origen.latitude+","+origen.longitude)
+                .appendQueryParameter("destination",destino.latitude+","+destino.longitude)
+                .appendQueryParameter("key", "AIzaSyDnnmgi40JjFpgC_tEywJ07ZZQiaoQ3Swk");
+        return builder.build().toString();
     }
 
 
@@ -94,4 +134,64 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /** A class to parse the Google Places in JSON format */
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String,String>>> >{
+
+        // Parsing the data in non-ui thread
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try{
+                jObject = new JSONObject(jsonData[0]);
+                DirectionsJSONParser parser = new DirectionsJSONParser();
+
+                // Starts parsing data
+                routes = parser.parse(jObject);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        // Executes in UI thread, after the parsing process
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+            ArrayList<LatLng> points = null;
+            PolylineOptions lineOptions = null;
+            MarkerOptions markerOptions = new MarkerOptions();
+
+            // Traversing through all the routes
+            for(int i=0;i<result.size();i++){
+                points = new ArrayList<LatLng>();
+                lineOptions = new PolylineOptions();
+
+                // Fetching i-th route
+                List<HashMap<String, String>> path = result.get(i);
+
+                // Fetching all the points in i-th route
+                for(int j=0;j<path.size();j++){
+                    HashMap<String,String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                // Adding all the points in the route to LineOptions
+                lineOptions.addAll(points);
+                lineOptions.width(2);
+                lineOptions.color(Color.RED);
+            }
+            // Drawing polyline in the Google Map for the i-th route
+            map.addPolyline(lineOptions);
+        }
+    }
+
+
 }
